67d9843af6685b4b8b95f1960e0690f3af5f8f39
Wake1st <36935801+Wake1st@users.noreply.github.com>
2025-02-06 17:06:11 -0700

    Updated sailing (#1)
    * controls update
    
    * camera controls and rigidbody physics
    
    * added more test level

--- boat.gd
+++ boat.gd
@@ -1,44 +1,45 @@
 class_name Boat
-extends Node3D
+extends RigidBody3D
 
 const MAX_RUDDER_ANGLE: float = PI/4
-const WIND_LOSS_ANGLE: float = PI*3/4
+const MAX_SAIL_ANGLE: float = PI/2
+const WIND_LOSS_ANGLE: float = PI*5/6
 
 @export_category("Control Surfaces")
 @export var rudderRateOfChange: float = 1.4
 @export var sailRateOfChange: float = 1.8
 
 @export_category("Kinematics")
-@export var rudderEfficiency: float = 0.4
-@export var sailEfficiency: float = 1.2
+@export var rudderEfficiency: float = 48
+@export var sailEfficiency: float = 64
 
 @onready var rudderRoot = %RudderRoot
 @onready var sailRoot = %SailRoot
 
 var currentRudderAngle: float = 0
-var currentSailVector: Vector2 = Vector2.UP
+var currentSailAngle: float = 0
 
 var windProjection: float
 
-
+#region External Methods
 func calculate_wind_projection(wind: Vector3) -> void:
 	var clampedAngle = clampf(wind.angle_to(-basis.z), 0, WIND_LOSS_ANGLE)
 	var windEffectiveness = (WIND_LOSS_ANGLE - clampedAngle) / WIND_LOSS_ANGLE
-	var directProjection = -wind.project(sailRoot.global_basis.x).length()
+	var directProjection = wind.project(sailRoot.global_basis.x).length()
 	windProjection = directProjection * windEffectiveness
-
+#endregion
 
 func _physics_process(delta: float) -> void:
 	input_processing(delta)
 	kinematics(delta)
 
-#region Inputs
+#region Input Handling
 func input_processing(delta: float) -> void:
-	var sailInput = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
+	var sailInput = Input.get_action_strength("sail_right") - Input.get_action_strength("sail_left")
 	var rudderInput = Input.get_action_strength("rudder_right") - Input.get_action_strength("rudder_left")
 	
-	if sailInput != Vector2.ZERO:
-		rotate_sail(delta * sailRateOfChange, sailInput)
+	if sailInput != 0.0:
+		rotate_sail(sailInput * delta * sailRateOfChange)
 	
 	if rudderInput != 0.0:
 		rotate_rudder(rudderInput * delta * rudderRateOfChange)
@@ -53,21 +54,20 @@ func rotate_rudder(deltaAngle: float) -> void:
 		currentRudderAngle = newAngle
 
 
-func rotate_sail(delta: float, inputVector: Vector2) -> void:
-	if currentSailVector != inputVector:
-		var targetSailAngle = currentSailVector.angle_to(inputVector)
-		var deltaAngle = targetSailAngle * delta
+func rotate_sail(deltaAngle: float) -> void:
+	var newAngle = clampf(currentSailAngle + deltaAngle, -MAX_SAIL_ANGLE, MAX_SAIL_ANGLE)
+	if currentSailAngle != newAngle:
 		sailRoot.rotate_y(deltaAngle)
 		
 		# store the updated angle
-		currentSailVector = currentSailVector.rotated(deltaAngle)
+		currentSailAngle = newAngle
 #endregion
 
 #region kinematics
 func kinematics(delta: float) -> void:
 	# rotate boat
-	rotate_y(-currentRudderAngle * delta * rudderEfficiency)
+	apply_torque(Vector3.DOWN * currentRudderAngle * delta * rudderEfficiency)
 	
 	# move forward
-	translate_object_local(Vector3(0.0, 0.0, windProjection) * delta * sailEfficiency)
+	apply_central_force(global_basis * Vector3.FORWARD * windProjection * delta * sailEfficiency)
 #endregion
---
+++ camera.gd
@@ -0,0 +1,11 @@
+class_name Camera
+extends Node3D
+
+@export var rotationRate: float = 1.0
+
+
+func _physics_process(delta):
+	var input = Input.get_action_strength("camera_right") - Input.get_action_strength("camera_left")
+	
+	if input != 0:
+		rotate_y(input * delta * rotationRate)
